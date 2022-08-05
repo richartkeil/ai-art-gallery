57d3047f3e60c64efbf5e8b4c5f7d32115c4a471
Richard Keil <8680858+richartkeil@users.noreply.github.com>
2022-08-05 19:10:32 +0200

    Clean up astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
 import vercel from '@astrojs/vercel/serverless';
 
-const integrations = [image(), tailwind(), astroImageTools];
-
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations,
+  integrations: [image(), tailwind(), astroImageTools]
 });
