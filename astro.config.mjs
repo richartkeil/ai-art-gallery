51f22a5035348bab386dc2ae1f1440cd20371d27
Richard Keil <8680858+richartkeil@users.noreply.github.com>
2022-08-05 19:19:10 +0200

    Disable Vercel adapter for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
   integrations: [image(), tailwind(), astroImageTools]
 });
